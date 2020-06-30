dfe8c3ad4efc252dae3f1f9aef42060fed77723f
Bolay Michael <michael.bolay@scantrust.com>
2020-06-30 15:38:03 +0200

    fix syntax

--- build-ios.sh
+++ build-ios.sh
@@ -1,18 +1,18 @@
 #! /bin/sh
 
-continue = 1
+continue=1
 
 if [[ -z "$IOS_LIBS_OUTPUT" ]]; then
     echo "Please set IOS_LIBS_OUTPUT to use this script."
     echo "   IOS_LIBS_OUTPUT: the path where the ios libraries are saved.";
-    continue = 0
+    continue=0
 fi
 
 if [[ -z "$IOS_TOOLCHAIN" ]]; then
     echo "Please set IOS_LIBS_OUTPUT to use this script."
     echo "   IOS_TOOLCHAIN: the path to the cmake iod toolchain.";
     echo "                  you can get it at https://github.com/leetal/ios-cmake"
-    continue = 0
+    continue=0
 fi
 
 if [[ $continue -ne 1 ]]; then
